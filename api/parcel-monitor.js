1c79a5eb175a3647d0e12be68b9cc3002def8aa4
White Red <rednblkx@protonmail.com>
2023-08-13 19:12:20 +0300

    parcel monitor api adjustments send notification after check not all at once

--- api/parcel-monitor.js
+++ api/parcel-monitor.js
@@ -29,49 +29,55 @@ export default async function handler(req, res) {
         let notifications = []
         for (const data of parcels_data.data) {
           try {
-            let shipment = parcels.find(a => a.tracking_id.includes(data?.awbNumber));
-            if (data && shipment.count_events < data?.eventsHistory.length) {
-              // const { data: dataS, error } = await supabase.from("subscriptions").select("*").eq("user_id", shipment.user_id)
-              for (const sub of shipment.subscriptions) {
-                notifications.push({ sub, data: { carrier: shipment.carrier.name, tracking_id: shipment.tracking_id, status: data.status, county: data?.eventsHistory[0].county } })
-              }
-              // axios.post(`https://ntfy.kodeeater.xyz/parcel-romania-${shipment.user_id.slice(0, 8)}`, `${shipment.carrier_id.name} \n ${shipment.tracking_id} - ${data?.data.status}, ${data?.data.eventsHistory[0].county}`)
-              if (data.statusId == 99 || data.statusId == 255) {
-                const { error: errorL } = await supabase.from("parcels_monitoring").delete().eq("tracking_id", shipment.tracking_id)
-                if (errorL) {
-                  console.log(errorL);
-                }
+            if (!data?.error) {
+              let shipment = parcels.find(a => a.tracking_id.includes(data?.awbNumber));
+              if (data && shipment?.count_events < data?.eventsHistory.length) {
+                // const { data: dataS, error } = await supabase.from("subscriptions").select("*").eq("user_id", shipment.user_id)
                 for (const sub of shipment.subscriptions) {
-                  notifications.push({ sub, data: { carrier: shipment.carrier.name, tracking_id: shipment.tracking_id, status: "Parcel delivered, removed from watching list!", county: "" } })
+                  webpush.sendNotification(sub, { carrier: shipment.carrier.name, tracking_id: shipment.tracking_id, status: data.status, county: data?.eventsHistory[0].county })
+                  notifications.push({ sub, data: { carrier: shipment.carrier.name, tracking_id: shipment.tracking_id, status: data.status, county: data?.eventsHistory[0].county } })
                 }
-              } else {
-                const { error: errorL } = await supabase.from("parcels_monitoring").update({ statusId: data?.statusId, last_updated: new Date(), last_checked: new Date(), count_events: data?.eventsHistory.length }).eq("tracking_id", shipment.tracking_id)
-                if (errorL) {
-                  console.log(errorL);
-                }
-              }
-            } else {
-              if (((new Date() - new Date(shipment.last_updated).getTime()) / (1000 * 3600 * 24)).toFixed(0) > 10) {
-                for (const sub of shipment.subscriptions) {
-                  notifications.push({sub, data: {carrier: shipment.carrier.name, tracking_id: shipment.tracking_id, status: "No updates in 10 days, removed from the watching list!", county: ""}})
-                }
-                const { error: errorL } = await supabase.from("parcels_monitoring").delete().eq("tracking_id", shipment.tracking_id)
-                if (errorL) {
-                  console.log(errorL);
-                }
-              } else {
+                // axios.post(`https://ntfy.kodeeater.xyz/parcel-romania-${shipment.user_id.slice(0, 8)}`, `${shipment.carrier_id.name} \n ${shipment.tracking_id} - ${data?.data.status}, ${data?.data.eventsHistory[0].county}`)
                 if (data.statusId == 99 || data.statusId == 255) {
                   const { error: errorL } = await supabase.from("parcels_monitoring").delete().eq("tracking_id", shipment.tracking_id)
                   if (errorL) {
                     console.log(errorL);
                   }
                   for (const sub of shipment.subscriptions) {
+                    webpush.sendNotification(sub, { carrier: shipment.carrier.name, tracking_id: shipment.tracking_id, status: "Parcel delivered, removed from watching list!", county: "" })
                     notifications.push({ sub, data: { carrier: shipment.carrier.name, tracking_id: shipment.tracking_id, status: "Parcel delivered, removed from watching list!", county: "" } })
                   }
                 } else {
-                  const { error: errorCheck } = await supabase.from("parcels_monitoring").update({ last_checked: new Date() }).eq("tracking_id", shipment.tracking_id)
-                  if (errorCheck) {
-                    console.log(errorCheck);
+                  const { error: errorL } = await supabase.from("parcels_monitoring").update({ statusId: data?.statusId, last_updated: new Date(), last_checked: new Date(), count_events: data?.eventsHistory.length }).eq("tracking_id", shipment.tracking_id)
+                  if (errorL) {
+                    console.log(errorL);
+                  }
+                }
+              } else {
+                if (((new Date() - new Date(shipment?.last_updated).getTime()) / (1000 * 3600 * 24)).toFixed(0) > 10) {
+                  for (const sub of shipment?.subscriptions) {
+                    webpush.sendNotification(sub, {carrier: shipment.carrier.name, tracking_id: shipment.tracking_id, status: "No updates in 10 days, removed from the watching list!", county: ""})
+                    notifications.push({sub, data: {carrier: shipment.carrier.name, tracking_id: shipment.tracking_id, status: "No updates in 10 days, removed from the watching list!", county: ""}})
+                  }
+                  const { error: errorL } = await supabase.from("parcels_monitoring").delete().eq("tracking_id", shipment.tracking_id)
+                  if (errorL) {
+                    console.log(errorL);
+                  }
+                } else {
+                  if (data?.statusId == 99 || data?.statusId == 255) {
+                    const { error: errorL } = await supabase.from("parcels_monitoring").delete().eq("tracking_id", shipment.tracking_id)
+                    if (errorL) {
+                      console.log(errorL);
+                    }
+                    for (const sub of shipment.subscriptions) {
+                      webpush.sendNotification(sub, { carrier: shipment.carrier.name, tracking_id: shipment.tracking_id, status: "Parcel delivered, removed from watching list!", county: "" })
+                      notifications.push({ sub, data: { carrier: shipment.carrier.name, tracking_id: shipment.tracking_id, status: "Parcel delivered, removed from watching list!", county: "" } })
+                    }
+                  } else {
+                    const { error: errorCheck } = await supabase.from("parcels_monitoring").update({ last_checked: new Date() }).eq("tracking_id", shipment.tracking_id)
+                    if (errorCheck) {
+                      console.log(errorCheck);
+                    }
                   }
                 }
               }
@@ -81,8 +87,7 @@ export default async function handler(req, res) {
           }
         }
       console.log(notifications.length);
-      console.log(notifications);
-        await Promise.all(notifications.map(b => webpush.sendNotification(b.sub, JSON.stringify(b.data))))
+        // await Promise.all(notifications.map(b => webpush.sendNotification(b.sub, JSON.stringify(b.data))))
     }
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader('Access-Control-Allow-Headers', 'authorization, content-type');
